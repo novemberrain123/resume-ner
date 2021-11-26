bbd44b5dced037dfa7a1d973502cd34fa68f315c
John <pleaseworkusob@gmail.com>
2021-11-26 23:56:41 +0800

    added files

---
+++ scrap.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[91]:
+
+
+import os
+import selenium
+from selenium import webdriver
+import time
+from PIL import Image
+import io
+import requests
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import ElementClickInterceptedException
+
+
+# In[92]:
+
+
+#Install Driver
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.maximize_window()
+
+
+# In[93]:
+
+
+#Specify Search URL 
+search_url="https://www.linkedin.com/login"
+
+driver.get(search_url)
+time.sleep(1)
+
+
+# In[94]:
+
+
+username = driver.find_element_by_id('username')
+password = driver.find_element_by_id('password')
+
+
+# In[95]:
+
+#PUT IN OWN LOGIN INFO
+username.send_keys("")
+password.send_keys("")
+
+
+# In[96]:
+
+
+username.submit()
+
+
+# In[97]:
+
+
+search_url="https://www.linkedin.com/search/results/people/?keywords={q}&origin=CLUSTER_EXPANSION&sid=~1J"
+
+#CHANGE q TO DIFFERENT VALUE, IT IS THE SEARCH TERM
+driver.get(search_url.format(q='lim'))
+time.sleep(1)
+
+
+# In[98]:
+
+#WHEN IT STARTS, POP UP WILL OCCUR FOR MULTIPLE FILE DOWNLOAD, CLICK ALLOW
+for i in range(10):
+    profile = driver.find_elements_by_css_selector(".app-aware-link[aria-hidden='false']")[i]
+    driver.execute_script("arguments[0].click();", profile)
+    time.sleep(1)
+    element = driver.find_element_by_css_selector("[aria-label='More actions']")
+    driver.execute_script("arguments[0].click();", element)
+    time.sleep(1)
+    element = driver.find_element_by_css_selector("[data-control-name='save_to_pdf']")
+    driver.execute_script("arguments[0].click();", element)
+    driver.back()
+    time.sleep(0.5)
+
+
+# In[ ]:
+
+
+
+
