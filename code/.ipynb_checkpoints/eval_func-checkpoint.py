bdfaf0d2c4d2731a7b89b94d27f50f6be7d25b97
John <pleaseworkusob@gmail.com>
2021-12-13 18:02:02 +0800

    remove gitignore ifles

--- code/.ipynb_checkpoints/eval_func-checkpoint.py
+++
@@ -1,6 +0,0 @@
-#refer to this https://github.com/chakki-works/seqeval
-
-from seqeval.metrics import classification_report
-
-def class_report(y_true, y_pred):
-    print(classification_report(y_true, y_pred))
