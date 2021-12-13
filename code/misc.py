f5dc842434891bea422d4d979fe5372a7885a159
ZI FENG <zifeng3553@outlook.com>
2021-12-13 19:45:44 +0800

    Update misc.py

--- code/misc.py
+++ code/misc.py
@@ -2,7 +2,7 @@ import os
 import re
 import sys
 import fitz
-from ner_spacy_pred import spacy_predicts
+from ner_spacy_pred import spacy_predict
 
 #parses search query and returns list of file indexes
 def search(q, ner):
