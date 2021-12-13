220aa45414650a60b9be565e8ab19f4af36ff256
zifeng53 <zifeng3553@outlook.com>
2021-12-14 01:00:47 +0800

    added .lower() function

--- code/ner_spacy_pred.py
+++ code/ner_spacy_pred.py
@@ -3,7 +3,7 @@ import spacy
 nlp_model = spacy.load('nlp_model')
 
 def spacy_predict(text):
-    test_doc = nlp_model(text) 
+    test_doc = nlp_model(text.lower()) 
     s = ""
     for ent in test_doc.ents:
         s += f'{ent.label_.upper():{30}}- {ent.text}' + '\n'
